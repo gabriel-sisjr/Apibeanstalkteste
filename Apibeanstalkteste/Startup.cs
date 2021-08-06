1c2f9dd6dd9fc1b160ec08c3b56297e85391189e
Gabriel Santana <gabriel.sistemasjr@hotmail.com>
2021-08-05 23:58:15 -0300

    Test route swagger

--- Apibeanstalkteste/Startup.cs
+++ Apibeanstalkteste/Startup.cs
@@ -33,13 +33,14 @@ namespace Apibeanstalkteste
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Apibeanstalkteste v1");
-                    c.RoutePrefix = string.Empty;
-                });
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Apibeanstalkteste v1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
